596cae6b8101587b8f700093b3ca9bbb98dc1510
Naren <Naren0625199@gmail.com>
2022-09-15 11:45:44 +0530

    first push

---
+++ src/main/java/com/narendran/learncourses/LearnCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.narendran.learncourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/narendran/learncourses/LearnCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narendran.learncourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
